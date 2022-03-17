c7e15b66fb8704ec9af30e8855009c3abf436f5d
dawegw2 <dawegwii@gmail.com>
2022-03-17 16:43:37 -0700

    update

--- venv/Scripts/activate.bat
+++ venv/Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set VIRTUAL_ENV=C:\GitHub\WilnerII_Daweg\100_Days\lab3\venv
+set VIRTUAL_ENV=C:\GitHub\EECS-1720-Lab3\venv
 
 if not defined PROMPT set PROMPT=$P$G
 
