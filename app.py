40fdb55def832272188025a91cbc50deb22502c2
dawegw2 <dawegwii@gmail.com>
2022-03-23 00:27:37 -0700

    Update app.py

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ from flask_socketio import SocketIO, send, emit
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'thisisasecret'
-socketio = SocketIO(app)
+socketio = SocketIO(app, cors_allowed_origins="*")
 
 @app.route("/")
 def index():
